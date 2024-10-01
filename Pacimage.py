e74ece3cf3940165c9b83c0fb3477a94936225fa
Gamer0020 <128790162+Gamer0020@users.noreply.github.com>
2024-10-01 13:12:05 +0200

    Not out of bound and fix of points

--- Pacimage.py
+++ Pacimage.py
@@ -10,7 +10,7 @@ image = pg.transform.scale(image, (100, 100))
 imagelocation = [0, 0]
 imagerotation = 90
 
-
+listepoints = [[True for i in range(6)] for i in range(8)]
 
 running = True
 while running:
@@ -21,29 +21,45 @@ while running:
             running = False
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_LEFT:
-                imagelocation[0] -= 100
+                if imagelocation[0] -100 >= 0:
+                    imagelocation[0] -= 100
                 image = pg.transform.rotate(image, imagerotation-270)
                 imagerotation = 270
 
             if event.key == pg.K_RIGHT:
-                imagelocation[0] += 100
+                if imagelocation[0] + 100 < WIDTH:
+                    imagelocation[0] += 100
                 image = pg.transform.rotate(image, imagerotation-90)
                 imagerotation = 90
 
             if event.key == pg.K_DOWN:
-                imagelocation[1] += 100
+                if imagelocation[1] + 100 < HEIGHT:
+                    imagelocation[1] += 100
                 image = pg.transform.rotate(image, imagerotation-180)
                 imagerotation = 180
 
             if event.key == pg.K_UP:
-                imagelocation[1] -= 100
+                if imagelocation[1] - 100 >= 0:
+                    imagelocation[1] -= 100
                 image = pg.transform.rotate(image, imagerotation)
                 imagerotation = 0
+            
+            if event.key == pg.K_SPACE:
+                for i in range(8):
+                    for j in range(6):
+                        listepoints[i][j] = True
+    x, y = imagelocation
+    x //= 100
+    y //= 100
+    listepoints[x][y] = False
+
+    for i in range(8):
+        for j in range(6):
+            if listepoints[i][j]:
+                pg.draw.circle(screen, pg.color.Color(200, 200, 200), (i*100+50, j*100+50), 10)
+                foundthing = True
 
     screen.blit(image, pg.Rect(imagelocation[0], imagelocation[1], 200, 200))
-    for i in range(5):
-        for j in range(8):
-            pg.draw.circle(screen, pg.color.Color(200, 200, 200), (i*100+50, j*100+50), 10)
     pg.display.update()
 
 pg.quit()
